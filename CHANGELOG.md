ee8f4bc95980e8436968f348c6120957d6034e40
Brian Cardarella <bcardarella@gmail.com>
2014-08-09 18:45:26 -0400

    Update addon for ember-cli 0.0.41

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,4 @@
 # master
 
 * [DEPRECATION] use `fulfilled` instead of `resolved` [#9](https://github.com/dockyard/ember-cli-async-button/pull/9)
+* Updated addon for ember-cli 0.0.41 [#10](https://github.com/dockyard/ember-cli-async-button/pull/10)
--- index.js
+++ index.js
@@ -1,36 +1,3 @@
-'use strict';
-
-var path = require('path');
-var fs   = require('fs');
-
-function EmberCLIAsyncButton(project) {
-  this.project = project;
-  this.name    = 'Ember CLI Async Button';
+module.exports = {
+  name: 'ember-cli-async-button'
 }
-
-function unwatchedTree(dir) {
-  return {
-    read:    function() { return dir; },
-    cleanup: function() { }
-  };
-}
-
-EmberCLIAsyncButton.prototype.treeFor = function treeFor(name) {
-  var treePath = path.relative(process.cwd(), __dirname);
-
-  if (name === 'templates') {
-      treePath = path.join(treePath, 'app', name);
-  } else {
-      treePath = path.join(treePath, name);
-  }
-
-  if (fs.existsSync(treePath)) {
-    return unwatchedTree(treePath);
-  }
-};
-
-EmberCLIAsyncButton.prototype.included = function included(app) {
-  this.app = app;
-};
-
-module.exports = EmberCLIAsyncButton;
