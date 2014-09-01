bf8686a0b166d797eb392e7db7caab21ed7af12f
Brian Cardarella <bcardarella@gmail.com>
2014-09-01 12:00:43 -0400

    Clean up test suite
    Refactored test suite to reduce code complexity. Added "contains"
    helper.

---
+++ tests/helpers/contains.js
@@ -0,0 +1,12 @@
+export default function(actual, expected, message) {
+  var text;
+  if (actual.text) {
+    text = actual.text();
+  } else {
+    text = actual;
+  }
+
+  var result = text.indexOf(expected) > -1;
+
+  QUnit.push(result, text, expected, message || 'contains');
+}
