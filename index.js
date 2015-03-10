2f3e6edd81a2731cccb5782644ad837b9c37ecf9
Robert Jackson <robert.w.jackson@me.com>
2015-03-10 09:38:32 -0400

    Provide helpful warning if not running Ember CLI 0.2.0 or above.

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 /* jshint node: true */
 'use strict';
 
+var checker = require('ember-cli-version-checker');
+
 module.exports = {
-  name: 'ember-cli-async-button'
+  name: 'ember-cli-async-button',
+
+  init: function() {
+    checker.assertAbove(this, '0.2.0');
+  }
 };
